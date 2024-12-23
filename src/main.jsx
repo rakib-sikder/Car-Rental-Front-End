9b45e53161bb87e0e5a6ace6000a3f7d1c2b7eac
rakib-sikder <itsmebroiam@gmail.com>
2024-12-23 23:29:26 +0600

    project setup done

---
+++ src/main.jsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Login from "./page/Login";
+import Register from "./page/Register";
+import NotFound from "./page/NotFound";
+import AvailableCars from "./page/AvailableCars";
+import AddCar from "./page/AddCar";
+import Home from "./page/Home";
+import MyCars from "./page/MyCars";
+import MyBookings from "./page/MyBookings";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[
+      
+      {
+        path: '/',
+        element: <Home></Home>
+        
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+        
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: "/available-cars",
+        element: <AvailableCars></AvailableCars>
+      },
+      {
+        path: "/add-car",
+        element: <AddCar></AddCar>
+      },
+      {
+        path: "/my-cars",
+        element: <MyCars></MyCars>
+      },
+      {
+        path: "/my-bookings",
+        element: <MyBookings></MyBookings>
+      },
+    ]
+  },
+  {
+    path: '*',
+    element:<NotFound></NotFound>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
